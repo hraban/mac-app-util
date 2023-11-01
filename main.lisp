403bca1aed0a14a5eb14fdd8e300b44a29f57145
Hraban Luyat <hraban@0brg.net>
2023-10-31 22:56:18 -0400

    refactor: use symbols for types

--- main.lisp
+++ main.lisp
@@ -11,11 +11,14 @@
 
 (in-package #:script)
 
-(defun leafp (type)
-  (member type '("bool" "string" "integer" "float") :test #'equal))
+(defparameter known-types '(string bool integer float array dictionary))
+(defparameter atom-types '(string bool integer float))
 
-(deftype leaf ()
-  `(satisfies leafp))
+(defparameter type-map
+  (mapcar (lambda (s) (cons (string-downcase (symbol-name s)) s)) known-types))
+
+(deftype atom-type ()
+  `(member ,@atom-types))
 
 (defun list-of-strings-p (l)
   (and (consp l) (every #'stringp l)))
@@ -23,29 +26,30 @@
 (deftype list-of-strings ()
   `(satisfies list-of-strings-p))
 
-(defun copy-atom (from to path type)
-  "Copy a single leaf value (string, boolean, ..) from one plist to the other"
-  (declare (type leaf type))
-  (let ((val (sh:run/ss `(plutil -extract ,path raw -expect ,type #\o - -- ,from))))
-    (sh:run `(plutil -replace ,path ,(format NIL "-~A" type) ,val -- ,to))))
+(defun copy-value (from to path)
+  "Copy a value from one plist to another"
+  (let ((type (get-type from path)))
+    (etypecase type
+      (atom-type
+       (let ((val (sh:run/ss `(plutil -extract ,path raw -expect ,type #\o - -- ,from))))
+         (sh:run `(plutil -replace ,path ,(format NIL "-~(~A~)" type) ,val -- ,to)))))))
 
 (defun get-type (plist path)
   (declare (type string plist)
            (type string path))
-  (sh:run/ss `(plutil -type ,path -o - -- ,plist)))
+  (let ((type (sh:run/ss `(plutil -type ,path -o - -- ,plist))))
+    (or (cdr (assoc type type-map :test #'equal))
+        (error "Unknown type ~A at ~A:~A" type plist path))))
 
-(defun copy-atoms (from to paths)
+(defun copy-paths (from to paths)
   (declare (type list-of-strings paths))
   (dolist (path paths)
-    (let ((type (get-type from path)))
-      (trivia:ematch type
-        ((satisfies leafp)
-         (copy-atom from to path type))))))
+    (copy-value from to path)))
 
 (defun main ()
   (trivia:match (uiop:command-line-arguments)
     ((list* from to paths)
-     (copy-atoms from to paths))
+     (copy-paths from to paths))
     (_ (error "Usage: app-plist-copy FROM TO PATH..."))))
 
 (main)
